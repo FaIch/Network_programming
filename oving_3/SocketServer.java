31e7c645848fd9ee2235ae473ba25976f29c6b21
Joakim Falch <joakimfalch@outlook.com>
2023-02-20 12:53:30 +0100

    added files for øving 4

--- oving_3/SocketServer.java
+++ oving_3/SocketServer.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.net.*;
 
 public class SocketServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         final int PORT_NR = 1250;
+         InetAddress inetAddress = InetAddress.getByName("192.168.0.77");
         int nrOfConnections = 0;
         try (ServerSocket serverSocket = new ServerSocket(PORT_NR)){
             System.out.println("Server is online");
